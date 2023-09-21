df982e3b33f98ce49c57a2acd658eacf78366502
Dipankar Kumar Roy <87216666+dipankarkumarroy@users.noreply.github.com>
2023-09-21 21:42:46 +0530

    Update c3.cpp

--- c3.cpp
+++ c3.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
+#include <iomanip>
 #include "include/Msolve.hpp"
 int main()
 {
-    std::cout << Msolve("(ab2+ d_y tc-r+2.0)", 1.0, 2, 3.5) << '\n';
+    std::cout << std::fixed << std::setprecision(10);
+    std::cout << Msolve("(2+67+(r+(-3-2-x-09))+2.00009+0.00000345)",34,6) << '\n';
 }
