d9d56ff2e5551c6401c24a16cb760e5eabed03b8
DIPANKAR KUMAR ROY <87216666+dipankarkumarroy@users.noreply.github.com>
2023-12-03 22:15:14 +0530

    Update c3.cpp

--- c3.cpp
+++ c3.cpp
@@ -5,4 +5,5 @@ int main()
 {
     std::cout << std::fixed << std::setprecision(10);
     std::cout << Msolve("(2+67+(r+(-3-2-x-09))+2.00009+0.00000345)",34,6) << '\n';
+    std::cout<<Msolve("2*(x^5) - 3*(x^4) + 7*(x^3) + 4*(x^2) - 6*(x*y) + 5*(y^3) - 8*(y^2) + 9*y - 12",2,-3);
 }
